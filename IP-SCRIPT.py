177721d1b202ff813a6ddf654cea308aca8737f1
Mixobixo7 <mixobixo7@gmail.com>
2022-05-01 22:47:28 +0200

    v1.0
    Script añadido, imagina que haces un repo con un script y te olvidas del script
    xD

---
+++ IP-SCRIPT.py
@@ -0,0 +1,87 @@
+import os, time
+from sys import stdout
+
+def red():
+    RED = "\033[1;31m"
+    stdout.write(RED)
+
+def green():
+    GREEN = "\033[0;32m"
+    stdout.write(GREEN)
+
+def blue():
+    BLUE = "\033[1;34m"
+    stdout.write(BLUE)
+
+def yellow():
+    YELLOW = "\033[1;33m"
+    stdout.write(YELLOW)
+
+def purple():
+    PURPLE = "\033[1;35m"
+    stdout.write(PURPLE)
+
+def white():
+    WHITE = "\033[1;37m"
+    stdout.write(WHITE)
+
+banner = """
+ ___ ____    ____   ____ ____  ___ ____ _____ 
+|_ _|  _ \  / ___| / ___|  _ \|_ _|  _ \_   _|
+ | || |_) | \___ \| |   | |_) || || |_) || |  
+ | ||  __/   ___) | |___|  _ < | ||  __/ | |    (Mixobixo_7 y x2Clicking)
+|___|_|     |____/ \____|_| \_\___|_|    |_|  
+                                             
+"""
+
+def menu():
+    red()
+    print(banner)
+    purple()
+    time.sleep(1)
+    print("1 -> Aplicar configuración")
+    time.sleep(1)
+    print("\n2 -> Borrar configuración")
+    time.sleep(1)
+    print("\n3 -> Salir")
+    time.sleep(1)
+
+    option = input("\n-->> ")
+
+    if option == "1":
+        apliconf()
+    if option == "2":
+        delconf()
+    if option == "3":
+        exit()
+
+def apliconf():
+    green()
+    os.system("sudo modprobe 8021q")
+    print("Selecciona IP y MS para la Vlan\n")
+    IP= input("Introduzca el CIDR: ")
+    VLAN= input("Vlan: ")
+    os.system(f"sudo vconfig add eth0 {VLAN}")
+    os.system(f"sudo ifconfig eth0.{VLAN} {IP} up")
+    
+    time.sleep(2)
+    print("Configuración aplicada correctamente")
+
+def delconf():
+    green()
+    print("Selecciona VLAN para borrar")
+    VLAN= input("Vlan para borrar: ")
+    os.system(f"sudo vconfig rem eth0.{VLAN}")
+    
+    print("\n Configuración borrada correctamente")
+
+if __name__ == '__main__':
+    id = os.getuid()
+    
+    if id == 0:
+        red()
+        print()
+        print("Funciona igual sin ser root ;D")
+        print()
+    else:
+        menu()
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # IP-SCRIPT
  Script para crear una vlan y asignarle una ip y máscara
+
+
+Modo de uso:
+Tiene un menú es sencillo.
+1 pones la configuración
+2 borras la vlan con su número
+3 sales
